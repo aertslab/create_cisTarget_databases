993d1ba9210177f8bb9df0015b0f2376249737c1
Gert Hulselmans <gert.hulselmans@kuleuven.vib.be>
2019-11-04 15:31:48 +0100

    Add support to take top CRM score for each gene ID by taking the maximum CRM score of all region IDs that belong to the same gene ID.
    Add support to take top CRM score for each gene ID by taking the maximum
    CRM score of all region IDs that belong to the same gene ID.
    
    Specify a regex for -g extract_gene_id_from_region_id_regex_replace which
    will remove the non-gene part of the regionID.
    
    Examples:
      - extract_gene_id_from_region_id_regex_replace = '#[0-9]+$'
          - region ID (input):
              "geneA#1", "geneB#1", "geneC#1", "geneA#2", "geneC#2"
          - gene IDs (output):
              "geneA", "geneB", "geneC"
      - extract_gene_id_from_region_id_regex_replace = '^.+@@'
          - region IDs (input):
              "region1@@geneA", "region2@@geneB", "region3@@geneA"
          - gene IDs (output):
              "geneA", "geneB"

--- src/make_feature_table.py
+++ src/make_feature_table.py
@@ -12,6 +12,7 @@ import argparse
 
 import io
 import os
+import re
 import sys
 import subprocess
 import numpy as np
@@ -45,9 +46,9 @@ def get_motif_name_to_filename_dict(motifs_dir, motifs_list_filename):
     return motif_name_to_filename_dict
 
 
-def get_region_ids_from_fasta(fasta_filename):
-    region_ids_list = list()
-    region_ids_set = set()
+def get_region_ids_or_gene_ids_from_fasta(fasta_filename, extract_gene_id_from_region_id_regex_replace):
+    gene_ids = set()
+    region_ids = set()
     duplicated_region_ids = False
 
     with open(fasta_filename, 'r') as fh:
@@ -56,27 +57,42 @@ def get_region_ids_from_fasta(fasta_filename):
                 # Get region ID by getting everything after '>' up till the first whitespace.
                 region_id = line[1:].split(maxsplit=1)[0]
 
-                # Check if all region IDs only appear once.
-                if region_id in region_ids_set:
-                    print(
-                        'Error: region ID "{0:s}" is not unique in FASTA file "{1:s}".'.format(
-                            region_id,
-                            fasta_filename
-                        ),
-                        file=sys.stderr
+                if extract_gene_id_from_region_id_regex_replace:
+                    # Extract gene ID from region ID.
+                    gene_id = re.sub(
+                        extract_gene_id_from_region_id_regex_replace,
+                        '',
+                        region_id
                     )
-                    duplicated_region_ids = True
 
-                region_ids_list.append(region_id)
-                region_ids_set.add(region_id)
+                    gene_ids.add(gene_id)
+                else:
+                    # Check if all region IDs only appear once.
+                    if region_id in region_ids:
+                        print(
+                            'Error: region ID "{0:s}" is not unique in FASTA file "{1:s}".'.format(
+                                region_id,
+                                fasta_filename
+                            ),
+                            file=sys.stderr
+                        )
 
-    if duplicated_region_ids:
-        sys.exit(1)
+                        duplicated_region_ids = True
 
-    return region_ids_list
+                    region_ids.add(region_id)
+
+    if extract_gene_id_from_region_id_regex_replace:
+        # Return gene IDs.
+        return sorted(gene_ids)
+    else:
+        if duplicated_region_ids:
+            sys.exit(1)
+
+        # Return region IDs.
+        return sorted(region_ids)
 
 
-def run_cluster_buster_for_motif(cluster_buster_path, fasta_filename, motif_filename, motif_name):
+def run_cluster_buster_for_motif(cluster_buster_path, fasta_filename, motif_filename, motif_name, extract_gene_id_from_region_id_regex_replace=None):
     # Score each region in FASTA file with Cluster-Buster
     # for motif and get top CRM score for each region.
     clusterbuster_command = [cluster_buster_path,
@@ -123,6 +139,30 @@ def run_cluster_buster_for_motif(cluster_buster_path, fasta_filename, motif_file
         dtype={'seq_name': str, 'crm_score': np.float32},
         engine='c'
     )
+
+    if extract_gene_id_from_region_id_regex_replace:
+        # Extract gene ID from the region ID by removing the non-gene part.
+        # Take the top CRM score for each gene ID by taking the maximum CRM
+        # score of all region IDs that belong to the same gene ID.
+        #
+        # Examples:
+        #   - extract_gene_id_from_region_id_regex_replace = '#[0-9]+$'
+        #       - region ID (input):
+        #           "geneA#1", "geneB#1", "geneC#1", "geneA#2", "geneC#2"
+        #       - gene IDs (output):
+        #           "geneA", "geneB", "geneC"
+        #   - extract_gene_id_from_region_id_regex_replace = '^.+@@'
+        #       - region IDs (input):
+        #           "region1@@geneA", "region2@@geneB", "region3@@geneA"
+        #       - gene IDs (output):
+        #           "geneA", "geneB"
+        crm_scores_df = crm_scores_df.assign(
+            gene_ids=crm_scores_df.index.str.replace(
+                extract_gene_id_from_region_id_regex_replace,
+                '',
+                regex=True
+            )
+        ).groupby('gene_ids').max()
 
     return motif_name, crm_scores_df
 
@@ -206,6 +246,19 @@ def main():
         help='Number of threads to use when scoring motifs. Default: 1.'
     )
 
+    parser.add_argument(
+        '-g',
+        '--genes',
+        dest='extract_gene_id_from_region_id_regex_replace',
+        action='store',
+        type=str,
+        required=False,
+        default=None,
+        help='Take top CRM score for a gene by taking the maximum CRM score of multiple regions for that gene. '
+        'Define a regex which will remove the non-gene part of the region ID, so only the gene ID remains. '
+        'Examples: "gene_id#some_number": "#[0-9]+$" or "region_id@@gene_id": "^.+@@".'
+    )
+
     args = parser.parse_args()
 
     if not os.path.exists(args.fasta_filename):
@@ -234,14 +287,17 @@ def main():
         motifs_list_filename=args.motifs_list_filename
     )
 
-    region_ids = get_region_ids_from_fasta(args.fasta_filename)
+    region_ids_or_gene_ids = get_region_ids_or_gene_ids_from_fasta(
+        args.fasta_filename,
+        args.extract_gene_id_from_region_id_regex_replace
+    )
 
-    # Create zeroed dataframe for all region IDs vs all motif names.
+    # Create zeroed dataframe for all region IDs or gene IDs vs all motif names.
     df_feature_table = pd.DataFrame(
-        data=np.zeros((len(region_ids),
+        data=np.zeros((len(region_ids_or_gene_ids),
                        len(motif_name_to_filename_dict)),
                       dtype=np.float32),
-        index=region_ids,
+        index=region_ids_or_gene_ids,
         columns=sorted(motif_name_to_filename_dict.keys())
     )
 
@@ -268,12 +324,17 @@ def main():
 
     with mp.Pool(processes=args.nbr_threads) as pool:
         for motif_name, motif_filename in motif_name_to_filename_dict.items():
-            pool.apply_async(func=run_cluster_buster_for_motif,
-                             args=[args.cluster_buster_path,
-                                   args.fasta_filename,
-                                   motif_filename,
-                                   motif_name],
-                             callback=add_crm_scores_for_motif_to_df_feature_table)
+            pool.apply_async(
+                func=run_cluster_buster_for_motif,
+                args=[
+                    args.cluster_buster_path,
+                    args.fasta_filename,
+                    motif_filename,
+                    motif_name,
+                    args.extract_gene_id_from_region_id_regex_replace
+                ],
+                callback=add_crm_scores_for_motif_to_df_feature_table
+            )
 
         # Prevents any more task from being submitted to the pool.
         pool.close()
@@ -281,7 +342,7 @@ def main():
         # Wait for worker processes to exit.
         pool.join()
 
-    def feature_table_write_tsv(df_feature_table, feature_table_output_filename, feature_table_output_format):
+    def feature_table_write_tsv(df_feature_table, feature_table_output_filename, feature_table_output_format, has_genes):
         """ Write feature table TSV file manually instead of with pandas. """
 
         # Get column names with all features.
@@ -297,16 +358,19 @@ def main():
         with open(feature_table_output_filename, 'w') as feature_table_output_fh:
             # Write header line.
             if feature_table_output_format == 'tsv':
-                print('regions', end='\t', file=feature_table_output_fh)
+                if has_genes:
+                    print('genes', end='\t', file=feature_table_output_fh)
+                else:
+                    print('regions', end='\t', file=feature_table_output_fh)
 
             print('\t'.join(column_names), end='\n', file=feature_table_output_fh)
 
             # Write each row.
             for row_idx in range(len(row_names)):
-                # Write region name.
+                # Write region ID or gene ID.
                 print(row_names[row_idx], end='\t', file=feature_table_output_fh)
 
-                # Write CRM scores for all features for the current region.
+                # Write CRM scores for all features for the current region or gene.
                 crm_scores[row_idx].tofile(file=feature_table_output_fh, sep='\t')
 
                 # Write newline.
@@ -320,9 +384,12 @@ def main():
             file=sys.stderr
         )
 
-        feature_table_write_tsv(df_feature_table=df_feature_table,
-                                feature_table_output_filename=args.feature_table_output_filename,
-                                feature_table_output_format=args.feature_table_output_format)
+        feature_table_write_tsv(
+            df_feature_table=df_feature_table,
+            feature_table_output_filename=args.feature_table_output_filename,
+            feature_table_output_format=args.feature_table_output_format,
+            has_genes=True if args.extract_gene_id_from_region_id_regex_replace else False
+        )
         # Faster than the following code:
         # df_feature_table.to_csv(
         #     path_or_buf=args.feature_table_output_filename,
@@ -341,9 +408,12 @@ def main():
             file=sys.stderr
         )
 
-        feature_table_write_tsv(df_feature_table=df_feature_table,
-                                feature_table_output_filename=args.feature_table_output_filename,
-                                feature_table_output_format=args.feature_table_output_format)
+        feature_table_write_tsv(
+            df_feature_table=df_feature_table,
+            feature_table_output_filename=args.feature_table_output_filename,
+            feature_table_output_format=args.feature_table_output_format,
+            has_genes=True if args.extract_gene_id_from_region_id_regex_replace else False
+        )
         # Faster than the following code:
         # df_feature_table.to_csv(
         #     path_or_buf=args.feature_table_output_filename,
