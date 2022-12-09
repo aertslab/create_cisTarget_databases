30a89d980badabb961b284bebc4e084b24aa6146
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2022-12-09 11:21:40 +0100

    Run black on feather_v1_or_v2.py.
    Run black on feather_v1_or_v2.py.

--- feather_v1_or_v2.py
+++ feather_v1_or_v2.py
@@ -29,21 +29,21 @@ def is_feather_v1_or_v2(feather_file: str) -> Optional[int]:
     :return: 1 (for Feather version 1), 2 (for Feather version 2) or None
     """
 
-    with open(feather_file, 'rb') as fh_feather:
+    with open(feather_file, "rb") as fh_feather:
         # Read first 6 and last 6 bytes to see if we have a Feather v2 file.
         fh_feather.seek(0, 0)
         feather_v2_magic_bytes_header = fh_feather.read(6)
         fh_feather.seek(-6, 2)
         feather_v2_magic_bytes_footer = fh_feather.read(6)
 
-        if feather_v2_magic_bytes_header == feather_v2_magic_bytes_footer == b'ARROW1':
+        if feather_v2_magic_bytes_header == feather_v2_magic_bytes_footer == b"ARROW1":
             return 2
 
         # Read first 4 and last 4 bytes to see if we have a Feather v1 file.
         feather_v1_magic_bytes_header = feather_v2_magic_bytes_header[0:4]
         feather_v1_magic_bytes_footer = feather_v2_magic_bytes_footer[2:]
 
-        if feather_v1_magic_bytes_header == feather_v1_magic_bytes_footer == b'FEA1':
+        if feather_v1_magic_bytes_header == feather_v1_magic_bytes_footer == b"FEA1":
             return 1
 
     return None
@@ -60,20 +60,22 @@ def get_all_column_names_from_feather(feather_file: str) -> List:
     feather_v1_or_v2 = is_feather_v1_or_v2(feather_file)
 
     if feather_v1_or_v2 == 1:
-        with open(feather_file, 'rb') as fh_feather:
+        with open(feather_file, "rb") as fh_feather:
             fh_feather.seek(-8, 2)
 
             # Get Feather v1 metadata length.
             metadata_length = np.frombuffer(fh_feather.read(4), dtype=np.int32)[0]
 
             # Read Feather v1 metadata.
-            fh_feather.seek(- (metadata_length + 8), 2)
-            feather_metadata = feather_v1_fbs.CTable.GetRootAs(bytearray(fh_feather.read(metadata_length)), 0)
+            fh_feather.seek(-(metadata_length + 8), 2)
+            feather_metadata = feather_v1_fbs.CTable.GetRootAs(
+                bytearray(fh_feather.read(metadata_length)), 0
+            )
 
             num_columns = feather_metadata.ColumnsLength()
 
             column_names = [
-                feather_metadata.Columns(column_idx).Name().decode('utf-8')
+                feather_metadata.Columns(column_idx).Name().decode("utf-8")
                 for column_idx in range(0, num_columns)
             ]
     elif feather_v1_or_v2 == 2:
