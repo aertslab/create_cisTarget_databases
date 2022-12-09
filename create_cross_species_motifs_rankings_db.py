cd65f0d80cdfc7a6cd949832a115f884fa405822
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2022-12-09 11:22:25 +0100

    Run black on create_cross_species_motifs_rankings_db.py.
    Run black on create_cross_species_motifs_rankings_db.py.

--- create_cross_species_motifs_rankings_db.py
+++ create_cross_species_motifs_rankings_db.py
@@ -18,23 +18,28 @@ from typing import List
 from cistarget_db import CisTargetDatabase, DatabaseTypes
 
 ct_rankings_db_motifs_vs_regions_or_genes_species_pattern = re.compile(
-    r'''^(?P<db_prefix_minimal>.*?)(\.(?P<species>(?!(part[^.]+[0-9]+)|cross_?species)[^.]+))\.(?P<db_type>(?P<motifs_or_tracks_type>motifs)_vs_(?P<regions_or_genes_type>regions|genes)\.rankings\.feather)$'''
+    r"""^(?P<db_prefix_minimal>.*?)(\.(?P<species>(?!(part[^.]+[0-9]+)|cross_?species)[^.]+))\.(?P<db_type>(?P<motifs_or_tracks_type>motifs)_vs_(?P<regions_or_genes_type>regions|genes)\.rankings\.feather)$"""
 )
 
 
-def create_cross_species_motifs_rankings_db(cross_species_db_prefix: str,
-                                            ct_rankings_db_motifs_vs_regions_or_genes_species_filenames: List[str]):
+def create_cross_species_motifs_rankings_db(
+    cross_species_db_prefix: str,
+    ct_rankings_db_motifs_vs_regions_or_genes_species_filenames: List[str],
+):
 
     cross_species_db_prefix = (
         cross_species_db_prefix
-        if cross_species_db_prefix.endswith('.cross_species')
-        else cross_species_db_prefix + '.cross_species'
+        if cross_species_db_prefix.endswith(".cross_species")
+        else cross_species_db_prefix + ".cross_species"
     )
 
-    start_creating_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time = time.monotonic()
+    start_creating_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time = (
+        time.monotonic()
+    )
     print(
         'Create cisTarget cross-species motifs rankings db from:\n  - "'
-        + '"\n  - "'.join(ct_rankings_db_motifs_vs_regions_or_genes_species_filenames) + '"\n'
+        + '"\n  - "'.join(ct_rankings_db_motifs_vs_regions_or_genes_species_filenames)
+        + '"\n'
     )
 
     ct_cross_species_rankings_db_motifs_vs_regions_or_genes = CisTargetDatabase.create_cross_species_rankings_db(
@@ -42,26 +47,26 @@ def create_cross_species_motifs_rankings_db(cross_species_db_prefix: str,
     )
 
     elapsed_creating_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time = (
-            time.monotonic() - start_creating_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time
+        time.monotonic()
+        - start_creating_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time
     )
     print(
-        f'Creating cisTarget cross-species '
-        f'{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind} vs '
-        f'{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind} rankings db took: '
-        f'{elapsed_creating_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time:.06f} seconds'
+        f"Creating cisTarget cross-species "
+        f"{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind} vs "
+        f"{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind} rankings db took: "
+        f"{elapsed_creating_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time:.06f} seconds"
     )
 
-    start_writing_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time = time.monotonic()
-    ct_cross_species_rankings_db_motifs_vs_regions_or_genes_filename = (
-        ct_cross_species_rankings_db_motifs_vs_regions_or_genes.create_db_filename_from_db_prefix(
-            db_prefix=cross_species_db_prefix,
-            extension='feather'
-        )
+    start_writing_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time = (
+        time.monotonic()
+    )
+    ct_cross_species_rankings_db_motifs_vs_regions_or_genes_filename = ct_cross_species_rankings_db_motifs_vs_regions_or_genes.create_db_filename_from_db_prefix(
+        db_prefix=cross_species_db_prefix, extension="feather"
     )
     print(
-        f'Writing cisTarget cross-species '
-        f'{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind} vs '
-        f'{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind} rankings db: '
+        f"Writing cisTarget cross-species "
+        f"{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind} vs "
+        f"{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind} rankings db: "
         f'"{ct_cross_species_rankings_db_motifs_vs_regions_or_genes_filename}"'
     )
 
@@ -71,29 +76,30 @@ def create_cross_species_motifs_rankings_db(cross_species_db_prefix: str,
     )
 
     elapsed_writing_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time = (
-            time.monotonic() - start_writing_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time
+        time.monotonic()
+        - start_writing_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time
     )
     print(
-        f'Writing cisTarget cross-species '
-        f'{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind} '
-        f'vs {ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind} rankings db '
-        f'took: {elapsed_writing_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time:.06f} seconds\n'
+        f"Writing cisTarget cross-species "
+        f"{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind} "
+        f"vs {ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind} rankings db "
+        f"took: {elapsed_writing_ct_cross_species_rankings_db_motifs_vs_regions_or_genes_time:.06f} seconds\n"
     )
 
-    start_writing_ct_cross_species_rankings_db_regions_or_genes_vs_motifs_time = time.monotonic()
-    ct_cross_species_rankings_db_regions_or_genes_vs_motifs_filename = \
-        DatabaseTypes.from_strings(
-            scores_or_rankings='rankings',
-            column_kind=ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind,
-            row_kind=ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind
-        ).create_db_filename(
-            db_prefix=cross_species_db_prefix,
-            extension='feather'
-        )
+    start_writing_ct_cross_species_rankings_db_regions_or_genes_vs_motifs_time = (
+        time.monotonic()
+    )
+    ct_cross_species_rankings_db_regions_or_genes_vs_motifs_filename = DatabaseTypes.from_strings(
+        scores_or_rankings="rankings",
+        column_kind=ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind,
+        row_kind=ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind,
+    ).create_db_filename(
+        db_prefix=cross_species_db_prefix, extension="feather"
+    )
     print(
-        f'Writing cisTarget cross-species '
-        f'{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind} vs '
-        f'{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind} rankings db: '
+        f"Writing cisTarget cross-species "
+        f"{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind} vs "
+        f"{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind} rankings db: "
         f'"{ct_cross_species_rankings_db_regions_or_genes_vs_motifs_filename}"'
     )
 
@@ -103,49 +109,50 @@ def create_cross_species_motifs_rankings_db(cross_species_db_prefix: str,
     )
 
     elapsed_writing_ct_cross_species_rankings_db_regions_or_genes_vs_motifs_time = (
-            time.monotonic() - start_writing_ct_cross_species_rankings_db_regions_or_genes_vs_motifs_time
+        time.monotonic()
+        - start_writing_ct_cross_species_rankings_db_regions_or_genes_vs_motifs_time
     )
     print(
-        f'Writing cisTarget cross-species '
-        f'{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind} vs '
-        f'{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind} rankings db '
-        f'took: {elapsed_writing_ct_cross_species_rankings_db_regions_or_genes_vs_motifs_time:.06f} seconds\n'
+        f"Writing cisTarget cross-species "
+        f"{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.row_kind} vs "
+        f"{ct_cross_species_rankings_db_motifs_vs_regions_or_genes.db_type.column_kind} rankings db "
+        f"took: {elapsed_writing_ct_cross_species_rankings_db_regions_or_genes_vs_motifs_time:.06f} seconds\n"
     )
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Create cisTarget cross-species motifs rankings databases.'
+        description="Create cisTarget cross-species motifs rankings databases."
     )
 
     parser.add_argument(
-        '-i',
-        '--input',
-        dest='input',
-        action='store',
+        "-i",
+        "--input",
+        dest="input",
+        action="store",
         type=str,
         required=True,
-        help='Input directory or database prefix with cisTarget motifs vs regions or genes rankings databases per '
-             'species.'
+        help="Input directory or database prefix with cisTarget motifs vs regions or genes rankings databases per "
+        "species.",
     )
 
     parser.add_argument(
-        '-o',
-        '--output',
-        dest='output_dir',
-        action='store',
+        "-o",
+        "--output",
+        dest="output_dir",
+        action="store",
         type=str,
         required=True,
-        help='Output directory to which the cisTarget cross-species motifs rankings database files will be written.'
+        help="Output directory to which the cisTarget cross-species motifs rankings database files will be written.",
     )
 
     args = parser.parse_args()
 
     # Construct glob string to find "*.*.motifs_vs_*.rankings.feather" cisTarget rankings database per species.
     ct_rankings_db_motifs_vs_regions_or_genes_species_glob_str = (
-        os.path.join(args.input, '*.*.motifs_vs_*.rankings.feather')
+        os.path.join(args.input, "*.*.motifs_vs_*.rankings.feather")
         if os.path.isdir(args.input)
-        else args.input + '*.*.motifs_vs_*.rankings.feather'
+        else args.input + "*.*.motifs_vs_*.rankings.feather"
     )
 
     # Get all "*.*.motifs_vs_*.rankings.feather" cisTarget databases in the input directory or the ones that start with
@@ -156,15 +163,17 @@ def main():
 
     if len(ct_rankings_db_motifs_vs_regions_or_genes_species_filenames) == 0:
         print(
-            f'Error: No cisTarget motifs vs regions or genes rankings databases per species found matching '
+            f"Error: No cisTarget motifs vs regions or genes rankings databases per species found matching "
             f'glob: "{ct_rankings_db_motifs_vs_regions_or_genes_species_glob_str}".',
-            file=sys.stderr
+            file=sys.stderr,
         )
         sys.exit(1)
 
     ct_dbs_hierarchical_dict = dict()
 
-    for ct_rankings_db_motifs_vs_regions_or_genes_species_filename in ct_rankings_db_motifs_vs_regions_or_genes_species_filenames:
+    for (
+        ct_rankings_db_motifs_vs_regions_or_genes_species_filename
+    ) in ct_rankings_db_motifs_vs_regions_or_genes_species_filenames:
         # Parse cisTarget database filename, so databases made from the same regions/genes, but lifted over
         # to different species can be grouped together.
         match = ct_rankings_db_motifs_vs_regions_or_genes_species_pattern.match(
@@ -172,28 +181,34 @@ def main():
         )
 
         if match:
-            db_prefix_minimal = match.group('db_prefix_minimal')
-            db_type = match.group('db_type')
-            species = match.group('species')
+            db_prefix_minimal = match.group("db_prefix_minimal")
+            db_type = match.group("db_type")
+            species = match.group("species")
 
             ct_dbs_hierarchical_dict.setdefault(db_prefix_minimal, dict())
             ct_dbs_hierarchical_dict[db_prefix_minimal].setdefault(db_type, dict())
-            ct_dbs_hierarchical_dict[db_prefix_minimal][db_type].setdefault(species, dict())
-            ct_dbs_hierarchical_dict[db_prefix_minimal][db_type][species] = ct_rankings_db_motifs_vs_regions_or_genes_species_filename
+            ct_dbs_hierarchical_dict[db_prefix_minimal][db_type].setdefault(
+                species, dict()
+            )
+            ct_dbs_hierarchical_dict[db_prefix_minimal][db_type][
+                species
+            ] = ct_rankings_db_motifs_vs_regions_or_genes_species_filename
 
     for db_prefix_minimal in ct_dbs_hierarchical_dict:
         for db_type in ct_dbs_hierarchical_dict[db_prefix_minimal]:
             # Get all cisTarget motifs vs regions or genes species rankings databases for the same regions/genes.
             ct_rankings_db_motifs_vs_regions_or_genes_species_filenames = [
                 ct_dbs_hierarchical_dict[db_prefix_minimal][db_type][species]
-                for species in sorted(ct_dbs_hierarchical_dict[db_prefix_minimal][db_type])
+                for species in sorted(
+                    ct_dbs_hierarchical_dict[db_prefix_minimal][db_type]
+                )
             ]
 
             create_cross_species_motifs_rankings_db(
                 cross_species_db_prefix=db_prefix_minimal,
-                ct_rankings_db_motifs_vs_regions_or_genes_species_filenames=ct_rankings_db_motifs_vs_regions_or_genes_species_filenames
+                ct_rankings_db_motifs_vs_regions_or_genes_species_filenames=ct_rankings_db_motifs_vs_regions_or_genes_species_filenames,
             )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
